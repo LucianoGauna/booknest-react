17e35913559748d125f51b5e812453bb9e7b6cb8
Luciano Gauna <luciano.927@hotmail.com>
2026-07-01 21:28:51 -0300

    feat: agregar a main el contexto de biblioteca creado

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import { LibraryProvider } from "./context/LibraryContext";
 import "primeicons/primeicons.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <LibraryProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </LibraryProvider>
     </AuthProvider>
   </StrictMode>,
 );

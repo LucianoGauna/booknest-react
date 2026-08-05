4c1a6826bf37114f76c0ba3ec45c103d90bae00b
Luciano Gauna <luciano.927@hotmail.com>
2026-08-04 22:26:12 -0300

    feat: agrega middleware de autenticacion y roles

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,15 @@
 import express from "express";
 import { login } from "../controllers/authController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/login", login);
+
+router.get("/profile", protect, (req, res) => {
+  res.json({
+    user: req.user,
+  });
+});
 
 export default router;
